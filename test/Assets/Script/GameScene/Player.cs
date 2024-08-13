2fda9bb384ee217164b42a4176b8970f2a522e97
land-34 <furi8landscape@gmail.com>
2024-08-13 17:41:54 +0900

    test

---
+++ test/Assets/Script/GameScene/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    FieldMaker fieldMaker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var raycastHitList = Physics.RaycastAll(ray).ToList();
+            if (raycastHitList.Any())
+            {
+                Vector3 hitPos = raycastHitList.First().point;
+                fieldMaker.Build(hitPos.x, hitPos.z, 2);
+            }
+        }
+    }
+}
